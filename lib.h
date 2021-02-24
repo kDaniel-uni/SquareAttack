d35fd8f55ec3f75d4b36dcbad5921437eec9fd47
Houssam Wassifi <houssam.wassif@etu.u-bordeaux.fr>
2021-02-24 23:58:02 +0100

    key expension

--- lib.h
+++ lib.h
@@ -7,13 +7,13 @@ for (j= 0;j < Nk;j+ +){
 }
 for (j=Nk;j <4(nr+ 1);j++){
   if (j%Nk== 0){
-    W[0, j] = W[0, j−Nk]⊕s(W[1, j−1])⊕RC[j/Nk];
+    W[0, j] = W[0, j−Nk]⊕SubByte(W[1, j−1])⊕RC[j/Nk];
     for (i= 1;i <4;i+ +){
-      W[i, j] =W[i, j−Nk]⊕s(W[i+ 1 % 4, j−1]);
+      W[i, j] =W[i, j−Nk]⊕SubByte(W[i+ 1 % 4, j−1]);
     }
   else if ( ( j % Nk) == 4 ){
     for (i= 0;i <4;i+ +){
-      W[i, j] =W[i, j−Nk]⊕s(W[i, j−1]);
+      W[i, j] =W[i, j−Nk]⊕SubByte(W[i, j−1]);
     }
   }
   else
@@ -22,3 +22,5 @@ for (j=Nk;j <4(nr+ 1);j++){
     }
   }
 }
+
+void SubByte () ;
