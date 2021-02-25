d1aa7cb26daed8d6adf1d22d0724fe187e582d37
Houssam Wassifi <houssam.wassif@etu.u-bordeaux.fr>
2021-02-25 01:02:35 +0100

    addrounkey

--- lib.h
+++ lib.h
@@ -24,3 +24,47 @@ for (j=Nk;j <4(nr+ 1);j++){
 }
 
 void SubByte () ;
+
+
+/* In  this  operation,  a  Round  Key  is  applied  to  the  State  by  a  simple  bitwise  EXOR.
+The  Round  Key is derived from the Cipher Key by means of the key schedule.
+The Round Key length is equal to the block length Nb.
+*/
+void AddRoundKey(unsigned int *state,unsigned int* RoundKey){
+  int Nb = 4; //peut etre egal à 4 6 ou 8
+  int round = 4;
+  int i,j;
+  for (i = 0; i < 4; ++i)
+  {
+    for (j = 0; j < 4; ++j)
+    {
+      (*state)[i][j] ^= RoundKey[(round * Nb * 4) + (i * Nb) + j];
+    }
+  }
+}
+
+
+
+
+
+Round(State,RoundKey)  {
+   ByteSub(State);
+   ShiftRow(State);
+   MixColumn(State);
+   AddRoundKey(State,RoundKey);
+}
+
+FinalRound(State,RoundKey)  {
+  ByteSub(State) ;
+  ShiftRow(State) ;
+  AddRoundKey(State,RoundKey);
+}
+
+AES (State,CipherKey)  {
+  KeyExpansion(CipherKey,ExpandedKey) ;
+  AddRoundKey(State,ExpandedKey);
+  For( i=1 ; i<Nr ; i++ ){
+    Round(State,ExpandedKey + Nb*i) ;
+  }
+  FinalRound(State,ExpandedKey + Nb*Nr);
+}
