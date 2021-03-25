c338cd5452b18083aad7d78c4bac7792c91284f7
Karl Daniel <karl.daniel@etu.u-bordeaux.fr>
2021-03-25 02:19:41 +0100

    finish implementation

--- keyExpension.h
+++ keyExpension.h
@@ -5,14 +5,25 @@
 #ifndef __KEYEXPENSION_H__
 #define __KEYEXPENSION_H__
 
-void KeyExpension();
+typedef char U8;
+typedef U8** tableau2D;
 
-void RotWord(unsigned char word[4]);
+void SBox(U8 word);
 
-void SubWord(unsigned char word[4]);
+void RotWord(U8 word[4]);
 
-void Rcon();
+void SubWord(U8 word[4]);
 
-void SBox(unsigned char word);
+U8* Rcon(unsigned int index);
+
+U8* KeyExpension(U8* key);
+
+tableau2D ParseKey(U8* key);
+
+U8* ParseTableau(tableau2D tableau);
+
+tableau2D NextRoundKey(tableau2D previousKey, int roundNumber);
+
+U8* ArrayXor(U8* first, U8* second);
 
 #endif
--- testKeyExpension.c
+++ testKeyExpension.c
@@ -4,15 +4,26 @@
 
 #include "keyExpension.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-	unsigned char test[4] = { 0x01, 0x2d, 0xc2, 0x9e };
+	char test[4] = { 0x01, 0x2d, 0xc2, 0x9e };
 	printf("%hhX %hhX %hhX %hhX\n", test[0], test[1], test[2], test[3]);
 	RotWord(test);
 	printf("%hhX %hhX %hhX %hhX\n", test[0], test[1], test[2], test[3]);
 
 	SubWord(test);
 	printf("%hhX %hhX %hhX %hhX\n", test[0], test[1], test[2], test[3]);
+
+	printf("\n\n");
+
+	char key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+	char* result = KeyExpension(key);
+
+	for (int index = 0; index < strlen(result); index++) {
+		printf("%hhX", result[index]);
+	}
+	printf("\n");
 
 	return 0;
 }
